d25ba5c2d815fcaad1347def054dd069dcf80365
IsaacAvila29 <isaac.avila.saenz29@gmail.com>
2026-02-25 09:54:05 -0700

    Refactor comments and transition table for clarity in finite state machine implementation

--- ceroUnoPat/ceroUnoPar.js
+++ ceroUnoPat/ceroUnoPar.js
@@ -1,8 +1,19 @@
+// Definición del autómata
+//Isaac Avila Saenz (238925)
+
+/**
+ * q0: estado inicial, acepta cadenas con número par de 1's y 0's
+ * q1: estado que representa un número impar de 1's y par de 0's
+ * q2: estado que representa un número par de 1's y impar de 0's
+ * q3: estado que representa un número impar de 1's y 0's
+ */
+
+//Tabla de transiciones basadas en el input y del estado actual
 const transitions = {
-  q0: { 0: "q2", 1: "q1" }, // pares ceros, pares unos
-  q1: { 0: "q3", 1: "q0" }, // pares ceros, impares unos
-  q2: { 0: "q0", 1: "q3" }, // impares ceros, pares unos
-  q3: { 0: "q1", 1: "q2" }, // impares ceros, impares unos
+  q0: { 0: "q2", 1: "q1" },
+  q1: { 0: "q3", 1: "q0" },
+  q2: { 0: "q0", 1: "q3" },
+  q3: { 0: "q1", 1: "q2" },
 };
 
 const estadoInicial = "q0";
@@ -13,7 +24,8 @@ function transicionExtendida(estado, input) {
   console.log("─".repeat(35));
 
   let estadoActual = estado;
-  for (const simbolo of input) {
+  for (let i = 0; i < input.length; i++) {
+    const simbolo = input[i];
     if (simbolo !== "0" && simbolo !== "1") {
       throw new Error(
         `Símbolo inválido: "${simbolo}". Solo se permiten "0" y "1".`,
